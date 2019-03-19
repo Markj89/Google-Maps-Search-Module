21f07666c830017c31b0487f1e4355f3f9c164e4
Markj89 <marcusj98@gmail.com>
2019-03-19 00:51:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google-Maps-Search-Module
+
+This application uses Google Maps API, Places API, Geocoding API, with our REST API to call each vendor in the vicindity of stops in the generated lead
---
+++ module.js
@@ -0,0 +1,14 @@
+/**
+ *	Web App Modules
+ *	Marcus Jackson, <marcusj98@gmail.com>
+ *	Load Web App JavaScript Dependencies/Plugins
+ **/
+define(["jquery", "angular", "angular-animate", "angularAMD", "angular-ui-router", "vsGoogleAutocomplete", "moment", "angular-moment", "ui"], function($, angular, angularAMD, moment) {
+	'use strict';
+	console.log("Initilization...");
+	var app = angular.module("vendorApp", ["ui.router", "vsGoogleAutocomplete", "ngAnimate", "angularMoment", "ui"]);
+	app.init = function() {
+		angular.bootstrap(document, ["vendorApp"]);
+	};
+	return app;
+});
